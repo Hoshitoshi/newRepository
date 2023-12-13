a136cad767a86bdffea42cc695f30d4d2fb36d6d
Toshi <to.hoshino@sutake.com>
2023-12-13 10:52:42 +0900

    コミット12/13-2 エラー修正

--- src/SortableContainer.tsx
+++ src/SortableContainer.tsx
@@ -2,10 +2,7 @@ import { useDroppable } from "@dnd-kit/core";
 import { rectSortingStrategy, SortableContext } from "@dnd-kit/sortable";
 import SortableItem from "./SortableItem";
 import React from "react";
-import { rightRow } from "./SortableContainer.css";
-
-
-// import styles from './Sortable.module.css'; // CSSモジュールのインポート
+// import { rightRow } from "./SortableContainer.css";
 
 const SortableContainer = ({
   id,
@@ -20,13 +17,12 @@ const SortableContainer = ({
     id,
   });
   return (
-    <div className={rightRow}>
-      <h3 className={rightRow}>{label}</h3>
+    <div className="w-[calc(33%-5px)]">
+      <h3 className="text-xl font-bold text-center">{label}</h3>
       <SortableContext id={id} items={items} strategy={rectSortingStrategy}>
         <div
           ref={setNodeRef}
-          className={rightRow}
-          // className="w-full border-2 border-gray-500/75 p-5 mt-2 rounded-md"
+          className="w-full border-2 border-gray-500/75 p-5 mt-2 rounded-md"
         >
           {items.map((id: string) => (
             <SortableItem key={id} id={id} />
